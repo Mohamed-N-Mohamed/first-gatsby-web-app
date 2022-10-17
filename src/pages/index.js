a641bc1da4a2dda4a3cd86947f24f73922032a6f
Mohamed Mohamed <63976154+Mohamed-N-Mohamed@users.noreply.github.com>
2022-10-17 17:42:15 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import * as React from "react"
-
 import { Link } from 'gatsby'
-
 import Layout from "../components/Layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Welcome to my first Gatsby site">
       <p>I'm learing Gatsby by following the Gatsby Tutorial</p>
+
+      <p>Used gatsby plugin to add image </p>
+
+      <StaticImage
+        alt="gundam image"
+        src="../images/gundam.jpg"
+      />
       </Layout>
       
     
